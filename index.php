0a994d862997a77a461b71f24cd26561af65aa53
kirandash <thekirankumardash@gmail.com>
2016-07-27 22:17:13 +0530

    5-The-Settings-API

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ Plugin URI: http://bgwebagency.com
 class KD_Options {
 	
 	public function __construct(){
-		
+		$this->register_settings_and_fields();
 	}
 	
 	public function add_menu_page(){
@@ -22,15 +22,50 @@ class KD_Options {
 		<div class="wrap">
             <h2>My theme Options</h2>
             <form method="post" action="options.php" enctype="multipart/form-data">
-            	<input>
+            	<?php settings_fields('kd_plugin_options');//includes all the hidden fields for security ?>
+                <?php do_settings_sections(__FILE__); ?>
             </form>
         </div>
 		<?php				
 	}
+	
+	public function register_settings_and_fields(){
+		register_setting('kd_plugin_options','kd_plugin_options');//3rd parameter = optional cb
+		//get_option('kd_plugin_options');	
+		add_settings_section('kd_main_section', 'Main Settings', array($this, 'kd_main_section_cb'), __FILE__);//id, title of section, cb, which page?
+		add_settings_field('kd_banner_heading','Banner Heading', array($this, 'kd_banner_heading_setting'), __FILE__, 'kd_main_section');
+		add_settings_field('kd_logo','Your Logo: ', array($this, 'kd_logo_setting'), __FILE__, 'kd_main_section');
+	}
+	
+	/*
+	 *
+	 * Inputs
+	 *
+	 */
+	 
+	 // Banner Heading
+	 public function kd_banner_heading_setting() {
+		 echo '<input>';
+	 }
+
+	 // Logo
+	 public function kd_logo_setting() {
+		 echo '<input type="file">';
+	 }
+	 	 
+	 //Function for validation and sanitization
+	 public function kd_main_section_cb(){
+		 //optional
+	 }
 	
 }
 
 add_action('admin_menu', function(){
 	KD_Options::add_menu_page();
 });
+
+add_action('admin_init', function(){
+	new KD_Options();
+});
+
 ?>
